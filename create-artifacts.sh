9eaf29b8af53e4ff491b1860ad8b25f43cf40a20
Ubuntu <ubuntu@hf-sv.us-central1-a.c.durable-bit-277615.internal>
2020-06-18 12:03:13 +0000

    Modified Commit

---
+++ create-artifacts.sh
@@ -0,0 +1,32 @@
+
+# chmod -R 0755 ./crypto-config
+# # Delete existing artifacts
+# rm -rf ./crypto-config
+# rm genesis.block mychannel.tx
+# rm -rf ../../channel-artifacts/*
+
+#Generate Crypto artifactes for organizations
+cryptogen generate --config=./artifacts/crypto-config.yaml --output=./artifacts/channel/crypto-config/
+
+
+
+# System channel
+SYS_CHANNEL="sys-channel"
+
+# channel name defaults to "mychannel"
+CHANNEL_NAME="mychannel"
+
+echo $CHANNEL_NAME
+
+# Generate System Genesis block
+configtxgen -profile OrdererGenesis -configPath ./artifacts -channelID $SYS_CHANNEL  -outputBlock ./artifacts/channel/genesis.block
+
+
+# Generate channel configuration block
+configtxgen -profile BasicChannel -configPath ./artifacts -outputCreateChannelTx ./artifacts/channel/mychannel.tx -channelID $CHANNEL_NAME
+
+echo "#######    Generating anchor peer update for Org1MSP  ##########"
+configtxgen -profile BasicChannel -configPath ./artifacts -outputAnchorPeersUpdate ./artifacts/channel/Org1MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org1MSP
+
+echo "#######    Generating anchor peer update for Org2MSP  ##########"
+configtxgen -profile BasicChannel -configPath ./artifacts -outputAnchorPeersUpdate ./artifacts/channel/Org2MSPanchors.tx -channelID $CHANNEL_NAME -asOrg Org2MSP
---
+++ up.sh
@@ -0,0 +1,6 @@
+./create-artifacts.sh
+
+docker-compose -f ./artifacts/docker-compose.yaml up -docker
+
+docker ps
+
